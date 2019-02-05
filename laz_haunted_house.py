5324780b0d961aba11762ce037cb2391ee7e051c
Lazaro Franco <lazaro300x@icloud.com>
2019-02-05 13:48:25 -0500

    Created file

---
+++ laz_haunted_house.py
@@ -0,0 +1,37 @@
+class Scene(obejct):
+
+    def enter(self):
+
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
+class Escape(Scene):
+
+    def enter(self):
+        pass
+
+class Basement(Scene):
+
+    def enter(self):
+        pass
+
+class UpStairs(Scene):
+
+    def enter(self):
+        pass
+
+class Flashlight(scene):
+
+    def enter(self):
+        pass
+
+class BedRoom(Scene):
+
+    def enter(self):
+        pass
